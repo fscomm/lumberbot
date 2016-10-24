1d2e090f6215335ca81acce88cf885ce705159ee
fscomm <Igor.Fedorishchev@homecredit.ru>
2016-10-25 00:32:09 +0300

    initial main stuff with PyMouseEvent

--- main.py
+++ main.py
@@ -1,21 +1,58 @@
-import win32api, win32con, win32gui
-import time, win32com.client
-import SendKeys
+import time
+from enum import Enum
+from pymouse import PyMouseEvent
+from pymouse import PyMouse
+from pykeyboard import PyKeyboard
 
-def click(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
+pymouse = PyMouse()
+pykeyboard = PyKeyboard()
 
-def openItNow(hwnd, windowText):
-    if windowText in win32gui.GetWindowText(hwnd):
-        win32gui.SetForegroundWindow(hwnd)
+class Tree(Enum):
+    LEFT = 1
+    RIGHT = 2
+
+leaf = Tree.LEFT
+
+def get_pixel_colour(i_x, i_y):
+    import PIL.Image # python-imaging
+    import PIL.ImageStat # python-imaging
+    import Xlib.display # python-xlib
+    o_x_root = Xlib.display.Display().screen().root
+    o_x_image = o_x_root.get_image(i_x, i_y, 1, 1, Xlib.X.ZPixmap, 0xffffffff)
+    o_pil_image_rgb = PIL.Image.fromstring("RGB", (1, 1), o_x_image.data, "raw", "BGRX")
+    lf_colour = PIL.ImageStat.Stat(o_pil_image_rgb).mean
+    return tuple(map(int, lf_colour))
+
+def tap():
+    global leaf
+    if leaf == Tree.LEFT:
+        pykeyboard.tap_key(pykeyboard.left_key)
+    else:
+        pykeyboard.tap_key(pykeyboard.right_key)
+
+def changing_position():
+    global leaf
+    x = 578
+    y = 872
+    if leaf == Tree.RIGHT:
+        x += 200
+    print(str(get_pixel_colour(x, y)))
 
 def main():
-    win32gui.EnumWindows(openItNow, 'Lumberjack')
-    shell = win32com.client.Dispatch("WScript.Shell")
-    shell.SendKeys("{SPACE}")
-    time.sleep(5)
+    global pymouse
+    global pykeyboard
+    global leaf
+
+    x_dim, y_dim = pymouse.screen_size()
+    pymouse.click(100, y_dim - 100, 1)
+
+    i = 0
+    while i < 8:
+        tap()
+        if changing_position():
+            leaf = Tree.RIGHT
+        i += 1
+        time.sleep(0.18)
 
 if __name__ == "__main__":
     main()
